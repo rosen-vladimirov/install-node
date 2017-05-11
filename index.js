c534858d7bc60a0eaca0b0f2637c40714c3ca777
tsvetie <tsvetelina.ivanova@telerik.com>
2017-05-11 15:01:18 +0300

    Ensure NS CLI

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const helpers = require("./lib/helpers");
+const ensureNodeServiceGenerator = require("./lib/ensure-node-service");
+const ensureCLIServiceGenerator = require("./lib/ensure-cli-service");
+
+const ensureNodeService = ensureNodeServiceGenerator(helpers);
+const ensureCLIService = ensureCLIServiceGenerator(helpers);
 const { platform } = process;
 
 let platformConfig;
@@ -14,7 +19,11 @@ switch (platform) {
         throw new Error(`Unsupported platform: ${platform}`);
 }
 
+const ensureNativeScript = () => {
+    return ensureNodeService.ensureNode(platformConfig)
+        .then(ensureCLIService.ensureCLI.bind(null, platformConfig));
+};
+
 module.exports = {
-    ensureNode: helpers.ensureNode.bind(null, platformConfig),
-    ensureCLI: helpers.ensureCLI
+    ensureNativeScript
 };
